89a21e721b686bce89a84ebced03f3f0e53b069a
Ausiàs Dalmau <ausi_dalmau@hotmail.com>
2022-10-02 12:52:21 +0200

    Image for Alcoro

--- Assets/Code/Items/Item.cs
+++ Assets/Code/Items/Item.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using Code.Basic;
 using Code.Boxes;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Code.Items
 {
     public class Item : TileParent<ItemTile>
     {
         [HideInInspector] public Box MyBox;
+        [SerializeField] public Image _image;
 
         public ItemTile[,] GetTiles()
         {
