cc6ca1c4c4b9e270f4652af21697ac0dd61228ad
MrDeku <sergi.p.crespo@gmail.com>
2022-11-26 13:52:05 +0100

    Sender and Burner particles Update

--- Assets/Code/Burners/Burner.cs
+++ Assets/Code/Burners/Burner.cs
@@ -5,10 +5,13 @@ namespace Code.Burners
     public class Burner : MonoBehaviour
     {
         [SerializeField] private AudioSource _burnerAudioSource;
+        [SerializeField] private GameObject _burnerParticles;
 
         public void DestroyThing()
         {
             _burnerAudioSource.Play();
+            _burnerParticles.SetActive(false);
+            _burnerParticles.SetActive(true);
         }
     }
 }
