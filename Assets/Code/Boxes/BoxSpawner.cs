8ea6579e97a64fb557a9fd79df09a09fb2a01d3f
MrDeku <sergi.p.crespo@gmail.com>
2022-11-26 13:52:38 +0100

    Box spawn position inside a range

--- Assets/Code/Boxes/BoxSpawner.cs
+++ Assets/Code/Boxes/BoxSpawner.cs
@@ -9,11 +9,17 @@ namespace Code.Boxes
         [SerializeField] private MovingBox _boxPrefab;
         [SerializeField] private GridCanvas _gridCanvas;
         [SerializeField] private AudioSource _spawnerAudioSource;
+        private Vector3 _positionCenter = new Vector3(130f, -40f, 0f);
+        private float range = 100f;
 
         public void OnPointerDown(PointerEventData eventData)
         {
             _spawnerAudioSource.Play();
-            Instantiate(_boxPrefab, Vector3.zero, _boxPrefab.transform.rotation, _gridCanvas.Canvas.transform);
+            MovingBox box = Instantiate(_boxPrefab, Vector3.zero, _boxPrefab.transform.rotation, _gridCanvas.Canvas.transform) as MovingBox;
+
+            Vector3 variation = new Vector3(Random.Range(-range, range), Random.Range(-range, range), 0f);
+            Vector3 newPos = _positionCenter + variation;
+            box.transform.localPosition = newPos;
         }
     }
 }
