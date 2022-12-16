8bb776cd35c26f6e0c1d9f7d90704c91fbf464e2
MrDeku <sergi.p.crespo@gmail.com>
2022-12-17 00:07:54 +0100

    Particles on click

---
+++ Assets/Code/Basic/PointerEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using UnityEngine;
+
+namespace Code.Basic
+{
+    public class PointerEffect : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem _particleSystem;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ActivatePS();
+            }
+        }
+
+        void ActivatePS()
+        {
+            Vector3 pos = Input.mousePosition;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            pos.z = 0f;
+            _particleSystem.transform.position = pos;
+
+            _particleSystem.Play();
+        }
+    }
+}
