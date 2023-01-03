6b8fcb8f427a76f41c569ac6949ad5d45206caeb
auusi9 <ausi_dalmau@hotmail.com>
2023-01-03 15:41:48 +0100

    Scene opening code

---
+++ Assets/Code/Menus/SceneOpening.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.Menus
+{
+    public class SceneOpening : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        
+        private readonly int _outHash = Animator.StringToHash("Out");
+        private readonly int _inHash = Animator.StringToHash("In");
+
+        private static bool _gameStarted;
+        
+        private void Awake()
+        {
+            if (_gameStarted)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+
+        private void OnEnable()
+        {
+            _animator.SetTrigger(_inHash);
+        }
+
+        public void StartGame()
+        {
+            _gameStarted = true;
+            _animator.SetTrigger(_outHash);
+            StartCoroutine(WaitForAnimationToFinish());
+        }
+        
+        private bool AnimationFinished()
+        {
+            return _animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_animator.IsInTransition(0);
+        }
+        
+        private IEnumerator WaitForAnimationToFinish()
+        {
+            yield return 0;
+            yield return new WaitUntil(AnimationFinished);
+            gameObject.SetActive(false);
+        }
+    }
+}
