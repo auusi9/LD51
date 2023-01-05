91e997dd593813981c5e6aeb4d650b91c1960eb1
MrDeku <sergi.p.crespo@gmail.com>
2023-01-05 14:32:26 +0100

    New PEANUTS!

--- Assets/Code/Items/FillSpawner.cs
+++ Assets/Code/Items/FillSpawner.cs
@@ -9,11 +9,20 @@ namespace Code.Items
         [SerializeField] private Item _itemPrefab;
         [SerializeField] private GridCanvas _gridCanvas;
         [SerializeField] private AudioSource _spawnerAudioSource;
+        [SerializeField] private Vector3 _positionCenter = new Vector3(130f, -40f, 0f);
+        [SerializeField] private float _range = 100f;
 
         public void OnPointerDown(PointerEventData eventData)
         {
             _spawnerAudioSource.Play();
-            Instantiate(_itemPrefab, Vector3.zero, _itemPrefab.transform.rotation, _gridCanvas.Canvas.transform);
+
+            Item fill = Instantiate(_itemPrefab, Vector3.zero, _itemPrefab.transform.rotation, _gridCanvas.Canvas.transform);
+
+            Vector3 variation = new Vector3(Random.Range(-_range, _range), Random.Range(-_range, _range), 0f);
+            Vector3 newPos = _positionCenter + variation;
+            fill.transform.localPosition = newPos;
+
+            //Instantiate(_itemPrefab, Vector3.zero, _itemPrefab.transform.rotation, _gridCanvas.Canvas.transform);
         }
     }
 }
