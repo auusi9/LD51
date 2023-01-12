f78c873af5d586c34bd3621cb1eea07e65db6c17
auusi9 <ausi_dalmau@hotmail.com>
2023-01-12 19:54:07 +0100

    Scene opening 3 audios

--- Assets/Code/Menus/SceneOpening.cs
+++ Assets/Code/Menus/SceneOpening.cs
@@ -8,6 +8,9 @@ namespace Code.Menus
     public class SceneOpening : MonoBehaviour
     {
         [SerializeField] private Animator _animator;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioSource _audioSource2;
+        [SerializeField] private AudioSource _audioSource3;
         
         private readonly int _outHash = Animator.StringToHash("Out");
         private readonly int _inHash = Animator.StringToHash("In");
@@ -25,6 +28,20 @@ namespace Code.Menus
         private void OnEnable()
         {
             _animator.SetTrigger(_inHash);
+        }
+
+        public void PlayAudioSource()
+        {
+            _audioSource.Play();
+        }
+        public void PlayAudioSource2()
+        {
+            _audioSource2.Play();
+        }
+        
+        public void PlayAudioSource3()
+        {
+            _audioSource3.Play();
         }
 
         public void StartGame()
